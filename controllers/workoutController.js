e897358ce7f3c23cdcbe17d85c2a2de4152edd1c
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-09-16 20:12:57 +0200

    Docs, routes and methods for workout endpoint

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -4,34 +4,62 @@ const userModel = require('../models/userModel');
 // Fetch all workouts
 const getWorkouts = async (req, res) => {
     const userId = req.query.user_id;
-    const status = req.query.status.charAt(0).toUpperCase() + req.query.status.slice(1).toLowerCase();
-  
+    const status = req.query.status?.match(/^(planned|in_progress|completed|skipped)$/i) ? req.query.status : null;
+
     try {
-      // Check if parameter is valid and user exists
-      if (userId) {
-        if (isNaN(userId)) {
-          return res.status(400).json({ error: 'Invalid user ID' });
+        // Check if parameter is valid and user exists
+        if (userId) {
+            if (isNaN(userId)) {
+                return res.status(400).json({error: 'Invalid user ID'});
+            }
+            const user = userModel.getUser(userId);
+            if (!user) {
+                res.status(404).json({error: 'User not found'});
+            }
         }
-        const user = userModel.getUser(userId);
-        if (!user) {
-          res.status(404).json({ error: 'User not found' });
+
+        const workouts = await workoutModel.getWorkouts(userId, status);
+
+        if (workouts) {
+            res.status(200).json(workouts);
+        } else {
+            res.status(404).json({error: 'Workouts not found'});
         }
-      }
-  
-      const workouts = await workoutModel.getWorkouts(userId, status);
-  
-      if (workouts) {
-        res.status(200).json(workouts);
-      } else {
-        res.status(404).json({ error: 'Workouts not found' });
-      }
     } catch (error) {
-      console.error('Error getting workouts:', error.stack);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error('Error getting workouts:', error.stack);
+        res.status(500).json({error: 'Internal server error'});
     }
-  };
+};
+
+const postWorkout = async (req, res) => {
+    // TODO: Implement POST /api/workouts
+
+    res.status(501).json({error: 'Not implemented'});
+};
+
+const getWorkoutById = async (req, res) => {
+    // TODO: Implement GET /api/workouts/{id}
+
+    res.status(501).json({error: 'Not implemented'});
+};
+
+const patchWorkout = async (req, res) => {
+    // TODO: Implement PATCH /api/workouts/{id}
+
+    res.status(501).json({error: 'Not implemented'});
+};
+
+const deleteWorkout = async (req, res) => {
+    // TODO: Implement DELETE /api/workouts/{id}
+
+    res.status(501).json({error: 'Not implemented'});
+};
 
 
-  module.exports = {
-    getWorkouts
-  };
+module.exports = {
+    getWorkouts,
+    getWorkoutById,
+    postWorkout,
+    patchWorkout,
+    deleteWorkout,
+};
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -5,12 +5,256 @@ const authController = require('../controllers/authController');
 
 // AUTH ROUTES
 router.get('/', authController.verifyToken, workoutController.getWorkouts);
+router.post('/', authController.verifyToken, workoutController.postWorkout);
+router.get('/:id', authController.verifyToken, workoutController.getWorkoutById);
+router.patch('/:id', authController.verifyToken, workoutController.patchWorkout);
+router.delete('/:id', authController.verifyToken, workoutController.deleteWorkout);
 
 module.exports = router;
 
-// ENDPOINT: POST /api/workouts
-// ENDPOINT: GET /api/workouts/:id
-// ENDPOINT: PATCH /api/workouts/:id
-// ENDPOINT: DELETE /api/workouts/:id
-
-// TODO: SWAGGER DOCS
+/**
+ * @swagger
+ * /api/workouts:
+ *   get:
+ *     summary: Get all workouts
+ *     description: This endpoint requires authorization token
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: query
+ *         name: user_id
+ *         required: false
+ *       - in: query
+ *         name: status
+ *         required: false
+ *         schema:
+ *           type: string
+ *           enum: [completed, in-progress, planned, skipped]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved all workouts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Workout'
+ *       401:
+ *         description: Unauthorized - Invalid or missing token
+ *       403:
+ *         description: Forbidden - Token does not have the required permissions
+ *       404:
+ *         description: Not Found - No workouts found
+ *   post:
+ *     summary: Create a new workout
+ *     description: This endpoint requires authorization token
+ *     tags: [Workouts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Workout'
+ *     responses:
+ *       201:
+ *         description: Successfully created a new workout
+ *       400:
+ *         description: Bad request - Invalid workout data
+ *       401:
+ *         description: Unauthorized - Invalid or missing token
+ * /api/workouts/{id}:
+ *   get:
+ *     summary: Get workout data
+ *     description: This endpoint requires authorization token
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Workout ID
+ *       - in: query
+ *         name: user_id
+ *         required: false
+ *         description: User ID
+ *       - in: query
+ *         name: status
+ *         required: false
+ *         description: Workout status
+ *         schema:
+ *           type: string
+ *           enum: [completed, in-progress, planned, skipped]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved workout data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Workout'
+ *       400:
+ *         description: Bad request - Invalid workout ID
+ *       401:
+ *         description: Unauthorized - Invalid or missing token
+ *       403:
+ *         description: Forbidden - Token does not have the required permissions
+ *       404:
+ *         description: Not Found - Workout not found
+ *   patch:
+ *     summary: Patch workout data
+ *     description: This endpoint requires authorization token, you can only patch workout for currently logged user
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Workout ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: 'Arm day'
+ *                 nullable: true
+ *               exercises:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     exercise:
+ *                       $ref: '#/components/schemas/Exercise'
+ *                     sets:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           reps:
+ *                             type: integer
+ *                             example: 10
+ *                             minimum: 0
+ *                           weight:
+ *                             type: float
+ *                             example: 75
+ *                             minimum: 0
+ *                             nullable: true
+ *                           order:
+ *                             type: integer
+ *                             example: 1
+ *                             minimum: 0
+ *                     order:
+ *                       type: integer
+ *                       example: 1
+ *                       minimum: 0
+ *               date:
+ *                 type: date
+ *                 example: 2024-09-08
+ *                 nullable: true
+ *               started_at:
+ *                 type: date
+ *                 example: 2024-09-08T10:00:00
+ *                 nullable: true
+ *               finished_at:
+ *                 type: date
+ *                 example: 2024-09-08T11:00:00
+ *                 nullable: true
+ *               notes:
+ *                 type: string
+ *                 example: 'Great workout!'
+ *     responses:
+ *       200:
+ *         description: Successfully updated workout data
+ *       400:
+ *         description: Bad request - Invalid workout ID
+ *       401:
+ *         description: Unauthorized - Invalid or missing token
+ *       403:
+ *         description: Forbidden - Token does not have the required permissions
+ *       404:
+ *         description: Not Found - Workout not found
+ *   delete:
+ *     summary: Delete workout
+ *     description: This endpoint requires authorization token, you can only delete workout for currently logged user
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Workout ID
+ *     responses:
+ *       200:
+ *         description: Successfully deleted workout
+ *       400:
+ *         description: Bad request - Invalid workout ID
+ *       401:
+ *         description: Unauthorized - Invalid or missing token
+ *       403:
+ *         description: Forbidden - Token does not have the required permissions
+ *       404:
+ *         description: Not Found - Workout not found
+ * components:
+ *   schemas:
+ *     Workout:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *           minimum: 0
+ *         related_workout_id:
+ *           type: integer
+ *           example: null
+ *         user_id:
+ *           type: integer
+ *           example: 1
+ *         name:
+ *           type: string
+ *           example: 'Arm day'
+ *         exercises:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               exercise:
+ *                 $ref: '#/components/schemas/Exercise'
+ *               sets:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     reps:
+ *                       type: integer
+ *                       example: 10
+ *                       minimum: 0
+ *                     weight:
+ *                       type: float
+ *                       example: 75
+ *                       minimum: 0
+ *                       nullable: true
+ *                     order:
+ *                       type: integer
+ *                       example: 1
+ *                       minimum: 0
+ *               order:
+ *                 type: integer
+ *                 example: 1
+ *                 minimum: 0
+ *         date:
+ *           type: date
+ *           example: 2024-09-08
+ *         started_at:
+ *           type: date
+ *           example: 2024-09-08T10:00:00
+ *           nullable: true
+ *         finished_at:
+ *           type: date
+ *           example: 2024-09-08T11:00:00
+ *           nullable: true
+ *         status:
+ *           type: string
+ *           example: completed
+ *           enum: [completed, in-progress, planned, skipped]
+ *         notes:
+ *           type: string
+ *           example: 'Great workout!'
+ */
