782909e0c890d30b7dabd807dee1553242976d0c
Patryk Haufa <suprenoctome@gmail.com>
2024-10-09 00:14:27 +0200

    Input data validation for equipment endpoints

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -1,4 +1,5 @@
 const equipmentModel = require('../models/equipmentModel');
+const equipmentSchema = require('../schemas/equipmentSchema');
 
 // Fetch all equipment
 const getEquipment = async (req, res) => {
@@ -19,14 +20,13 @@ const getEquipment = async (req, res) => {
 
 // Fetch single equipment by its ID
 const getEquipmentById = async (req, res) => {
-  const equipment_id = Number(req.params.id);
-
-  // Validate equipment ID
-  if (isNaN(equipment_id) || equipment_id <= 0) return res.status(400).json({ error: 'Invalid equipment ID' });
+  // Validate params data
+  const { error } = equipmentSchema.getEquipmentSchema.validate(req.params);
+  if (error) return res.status(400).json({ error: error.details[0].message });
 
   try {
     // Fetch equipment from database
-    const equipment = await equipmentModel.getEquipmentById(equipment_id);
+    const equipment = await equipmentModel.getEquipmentById(req.params.id);
 
     // Check if anything was returned
     if (!equipment) return res.status(404).json({ error: 'Equipment not found' });
---
+++ schemas/equipmentSchema.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+
+// Base validation schema for user data
+const baseEquipmentSchema = {
+  id: Joi.number()
+    .integer()
+    .positive()
+    .messages({
+      'number.base': 'ID must be a number',
+      'number.integer': 'ID must be an integer',
+      'number.positive': 'ID must be a positive number'
+    }),
+
+  name: Joi.string()
+    .min(3)
+    .max(100)
+    .pattern(/^(?!.*\s{2,})[A-Za-z0-9]+(?: [A-Za-z0-9]+)*$/)
+    .messages({
+      'string.min': 'Equipment name must be at least 3 characters long',
+      'string.max': 'Equipment name must be at most 100 characters long',
+      'string.pattern.base': 'Equipment name must contain only letters, numbers, and spaces. You are required to use atleast 3 alphanumeric symbols and can only use one spacebar between words.'
+    })
+};
+
+// Specific validation schema for fetching single equipment by its id
+const getEquipmentSchema = Joi.object({
+  id: baseEquipmentSchema.id.required().messages({ 'any.required': 'ID is required' })
+});
+
+module.exports = {
+  getEquipmentSchema
+};
