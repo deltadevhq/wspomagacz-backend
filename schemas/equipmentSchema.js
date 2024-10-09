bfe6b351834c02b63c6057897ade5a6178544370
Patryk Haufa <suprenoctome@gmail.com>
2024-10-10 00:26:40 +0200

    Fix validation

--- schemas/equipmentSchema.js
+++ schemas/equipmentSchema.js
@@ -5,26 +5,30 @@ const baseEquipmentSchema = {
   id: Joi.number()
     .integer()
     .positive()
+    .required()
     .messages({
       'number.base': 'ID must be a number',
       'number.integer': 'ID must be an integer',
-      'number.positive': 'ID must be a positive number'
+      'number.positive': 'ID must be a positive number',
+      'any.required': 'ID is required'
     }),
 
   name: Joi.string()
     .min(3)
     .max(100)
     .pattern(/^(?!.*\s{2,})[A-Za-z0-9]+(?: [A-Za-z0-9]+)*$/)
+    .required()
     .messages({
       'string.min': 'Equipment name must be at least 3 characters long',
       'string.max': 'Equipment name must be at most 100 characters long',
-      'string.pattern.base': 'Equipment name must contain only letters, numbers, and spaces. You are required to use atleast 3 alphanumeric symbols and can only use one spacebar between words.'
+      'string.pattern.base': 'Equipment name must contain only letters, numbers, and spaces. You are required to use atleast 3 alphanumeric symbols and can only use one spacebar between words.',
+      'any.required': 'Equipment name is required'
     })
 };
 
 // Specific validation schema for fetching single equipment by its id
 const getEquipmentSchema = Joi.object({
-  id: baseEquipmentSchema.id.required().messages({ 'any.required': 'ID is required' })
+  id: baseEquipmentSchema.id
 });
 
 module.exports = {
--- schemas/muscleSchema.js
+++ schemas/muscleSchema.js
@@ -5,26 +5,30 @@ const baseMuscleSchema = {
   id: Joi.number()
     .integer()
     .positive()
+    .required()
     .messages({
       'number.base': 'ID must be a number',
       'number.integer': 'ID must be an integer',
-      'number.positive': 'ID must be a positive number'
+      'number.positive': 'ID must be a positive number',
+      'any.required': 'ID is required'
     }),
 
   name: Joi.string()
     .min(3)
     .max(100)
     .pattern(/^(?!.*\s{2,})[A-Za-z0-9]+(?: [A-Za-z0-9]+)*$/)
+    .required()
     .messages({
       'string.min': 'Muscle name must be at least 3 characters long',
       'string.max': 'Muscle name must be at most 100 characters long',
-      'string.pattern.base': 'Muscle name must contain only letters, numbers, and spaces. You are required to use atleast 3 alphanumeric symbols and can only use one spacebar between words.'
+      'string.pattern.base': 'Muscle name must contain only letters, numbers, and spaces. You are required to use atleast 3 alphanumeric symbols and can only use one spacebar between words.',
+      'any.required': 'Muscle name is required'
     })
 };
 
 // Specific validation schema for fetching single muscle by its id
 const getMuscleSchema = Joi.object({
-  id: baseMuscleSchema.id.required().messages({ 'any.required': 'ID is required' })
+  id: baseMuscleSchema.id
 });
 
 module.exports = {
