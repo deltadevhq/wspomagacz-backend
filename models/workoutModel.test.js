11b69953f926aafc5883870485e9e55fa8654a0f
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-10-24 08:45:35 +0200

    Separate creation and edition tests

--- models/workoutModel.test.js
+++ models/workoutModel.test.js
@@ -15,104 +15,107 @@ const createTestWorkout = async () => {
   return await createWorkout(related_workout_id, user_id, name, exercises, date, notes);
 };
 
-describe('Workout Creation', () => {
-  let workout = undefined;
+describe('Workout Model', () => {
+  describe('Creating workout', () => {
+    // TODO: Separate past and future workouts creation tests
+    test('successfully creates a workout', async () => {
+      const newWorkout = await createTestWorkout();
+      expect(newWorkout).toHaveProperty('id');
 
-  beforeEach(async () => {
-    expect(workout).toBeUndefined();
+      const date = new Date(newWorkout.date);
+      console.log('Date:', date.toLocaleDateString('sv-SE'));
 
-    workout = await createTestWorkout();
-    workout.exercises = JSON.stringify(workout.exercises);
-  });
-
-  afterEach(async () => {
-    await deleteWorkout(workout.id);
-
-    // Reset the workout after deleting it to make sure it's not used in the next test
-    workout = undefined;
-  });
-
-  // TODO: Separate past and future workouts creation tests
-  test('successfully creates a workout', async () => {
-    // 2 workouts are created when using this test
-
-    const newWorkout = await createTestWorkout();
-    expect(newWorkout).toHaveProperty('id');
-
-    const date = new Date(newWorkout.date);
-    console.log('Date:', date.toLocaleDateString('sv-SE'));
-
-    // If the date is in the past the status should be 'skipped'
-    if (date < new Date().setHours(0, 0, 0, 0)) {
-      expect(newWorkout.status).toBe('skipped');
-    }
-
-    // If the date is today or in the future the status should be 'planned'
-    if (date >= new Date().setHours(0, 0, 0, 0)) {
-      expect(newWorkout.status).toBe('planned');
-    }
-  });
-
-  test('fails to create a workout with a date that already has a workout', async () => {
-    // Try to create a workout with the same date as the workout created in the hook
-    await expect(createWorkout(null, workout.user_id, 'Test workout', workout.exercises, workout.date, 'Test notes')).rejects.toThrowError();
-  });
-
-  test('successfully updates a workout', async () => {
-    const { exercises, date, notes } = workout;
-    expect(workout.name).not.toBe('Updated workout');
-
-    const updatedWorkout = await updateWorkout(workout.id, 'Updated workout', exercises, date, notes);
-    expect(updatedWorkout.name).toBe('Updated workout');
-  });
-
-  test('fails to update a workout date to a date that already has a workout', async () => {
-    // Create a workout to be used for the test
-    const testWorkout = await createTestWorkout();
-    const { id, name, notes } = testWorkout;
-    const exercises = JSON.stringify(testWorkout.exercises);
-
-    // Try to update the workout to the same date as the workout created in the hook
-    await expect(updateWorkout(id, name, exercises, workout.date, notes)).rejects.toThrowError();
-  });
-
-  test('handles starting a workout', async () => {
-    expect(workout.started_at).toBeNull();
-
-    if (workout.date === new Date().toLocaleDateString('sv-SE')) {
-      expect(workout.status).toBe('planned');
-
-      const startedWorkout = await startWorkout(workout.id);
-
-      expect(startedWorkout.started_at).not.toBeNull();
-      expect(startedWorkout.status).toBe('in_progress');
-    } else {
-      // If the date is in the past the workout should be skipped, therefore it should not be possible to start it
-      if (workout.date < new Date().setHours(0, 0, 0, 0)) {
-        expect(workout.status).toBe('skipped');
+      // If the date is in the past the status should be 'skipped'
+      if (date < new Date().setHours(0, 0, 0, 0)) {
+        expect(newWorkout.status).toBe('skipped');
       }
 
-      expect(workout.status).toBe('planned');
+      // If the date is today or in the future the status should be 'planned'
+      if (date >= new Date().setHours(0, 0, 0, 0)) {
+        expect(newWorkout.status).toBe('planned');
+      }
+    });
 
-      // If the date is not today it should not be possible to start the workout and an error should be thrown
-      await expect(startWorkout(workout.id)).rejects.toThrowError();
-    }
+    test('fails to create a workout with a date that already has a workout', async () => {
+      const { related_workout_id, user_id, exercises, date, notes } = await createTestWorkout();
+
+      await expect(createWorkout(related_workout_id, user_id, 'Test workout', exercises, date, notes)).rejects.toThrowError();
+    });
   });
 
-  test('handles finishing a workout', async () => {
-    expect(workout.finished_at).toBeNull();
+  describe('Editing workout', () => {
+    let workout = undefined;
 
-    // Try prematurely finishing a workout, it should always result in an error
-    await expect(finishWorkout(workout.id)).rejects.toThrowError();
+    beforeEach(async () => {
+      expect(workout).toBeUndefined();
 
-    // Start the workout, the steps are thoroughly tested in the 'handles starting a workout' test
-    const startedWorkout = await startWorkout(workout.id);
-    expect(startedWorkout.started_at).not.toBeNull();
-    expect(startedWorkout.status).toBe('in_progress');
+      workout = await createTestWorkout();
+      workout.exercises = JSON.stringify(workout.exercises);
+    });
 
-    // Finish the workout
-    const finishedWorkout = await finishWorkout(workout.id);
-    expect(finishedWorkout.finished_at).not.toBeNull();
-    expect(finishedWorkout.status).toBe('completed');
+    afterEach(async () => {
+      await deleteWorkout(workout.id);
+
+      // Reset the workout after deleting it to make sure it's not used in the next test
+      workout = undefined;
+    });
+
+    test('successfully updates a workout', async () => {
+      const { exercises, date, notes } = workout;
+      expect(workout.name).not.toBe('Updated workout');
+
+      const updatedWorkout = await updateWorkout(workout.id, 'Updated workout', exercises, date, notes);
+      expect(updatedWorkout.name).toBe('Updated workout');
+    });
+
+    test('fails to update a workout date to a date that already has a workout', async () => {
+      // Create a workout to be used for the test
+      const testWorkout = await createTestWorkout();
+      const { id, name, notes } = testWorkout;
+      const exercises = JSON.stringify(testWorkout.exercises);
+
+      // Try to update the workout to the same date as the workout created in the hook
+      await expect(updateWorkout(id, name, exercises, workout.date, notes)).rejects.toThrowError();
+    });
+
+    test('handles starting a workout', async () => {
+      expect(workout.started_at).toBeNull();
+
+      if (workout.date === new Date().toLocaleDateString('sv-SE')) {
+        expect(workout.status).toBe('planned');
+
+        const startedWorkout = await startWorkout(workout.id);
+
+        expect(startedWorkout.started_at).not.toBeNull();
+        expect(startedWorkout.status).toBe('in_progress');
+      } else {
+        // If the date is in the past the workout should be skipped, therefore it should not be possible to start it
+        if (workout.date < new Date().setHours(0, 0, 0, 0)) {
+          expect(workout.status).toBe('skipped');
+        }
+
+        expect(workout.status).toBe('planned');
+
+        // If the date is not today it should not be possible to start the workout and an error should be thrown
+        await expect(startWorkout(workout.id)).rejects.toThrowError();
+      }
+    });
+
+    test('handles finishing a workout', async () => {
+      expect(workout.finished_at).toBeNull();
+
+      // Try prematurely finishing a workout, it should always result in an error
+      await expect(finishWorkout(workout.id)).rejects.toThrowError();
+
+      // Start the workout, the steps are thoroughly tested in the 'handles starting a workout' test
+      const startedWorkout = await startWorkout(workout.id);
+      expect(startedWorkout.started_at).not.toBeNull();
+      expect(startedWorkout.status).toBe('in_progress');
+
+      // Finish the workout
+      const finishedWorkout = await finishWorkout(workout.id);
+      expect(finishedWorkout.finished_at).not.toBeNull();
+      expect(finishedWorkout.status).toBe('completed');
+    });
   });
 });
