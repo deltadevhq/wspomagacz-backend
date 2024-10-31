c1b52f56a4b787a9c549250eeed7a5e897de0c2e
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-10-31 08:38:27 +0100

    Fix pool

--- config/database.js
+++ config/database.js
@@ -1,4 +1,11 @@
-const { databaseHost, databasePort, databaseName, databaseUser, databasePassword, applicationTimezone } = require('./settings');
+const {
+  databaseHost,
+  databasePort,
+  databaseName,
+  databaseUser,
+  databasePassword,
+  applicationTimezone,
+} = require('./settings');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,26 +14,25 @@ const pool = new Pool({
   database: databaseName,
   user: databaseUser,
   password: databasePassword,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
 
 // Initialize connection and return a Promise
 const initDBConnection = async () => {
-  const client = await pool.connect();
   try {
-    await client.query('SELECT NOW()');
+    await pool.query('SELECT NOW()');
     console.log(`Database connection successful!`);
 
-    const timezoneResult = await client.query('SHOW TIMEZONE');
-    if (timezoneResult.rows[0].TimeZone !== applicationTimezone) {
-      console.error(`Database timezone (${timezoneResult.rows[0].TimeZone}) is not equal to timezone from configuration (${applicationTimezone}).`)
+    const timezoneResult = await pool.query('SHOW TIMEZONE');
+    const { TimeZone } = timezoneResult.rows[0];
+
+    if (TimeZone !== applicationTimezone) {
+      console.error(`Database timezone (${TimeZone}) is not equal to timezone from configuration (${applicationTimezone}).`);
       process.exit(-1);
     }
   } catch (error) {
     console.error('Error executing query', error.stack);
     throw error;
-  } finally {
-    client.release();
   }
 };
 
