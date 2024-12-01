db7bed483ea968e35fdd8c62ab3f6adf446b459f
Patryk Haufa <suprenoctome@gmail.com>
2024-12-01 17:11:43 +0100

    Change route to /api/version

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use('/api/notifications', dateFormatter, routes.notificationRoutes);
 app.use('/api/users', dateFormatter, routes.userRoutes);
 app.use('/api/workouts', dateFormatter, routes.workoutRoutes);
 
-app.get('/version', (req, res) => {
+app.get('/api/version', (req, res) => {
   res.json({ name: packageJson.name, version: packageJson.version });
 });
 
